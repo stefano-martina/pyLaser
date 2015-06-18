f33d45627e8c443bcea8e65f1d98b4a7d94de7fb
Stefano Martina <stefano.martina@gmail.com>
2015-06-18 16:27:40 +0200

    Added small functionalities to MilonniAni

--- milonniAni.py
+++ milonniAni.py
@@ -8,10 +8,16 @@ from pylab import *
 G = 2      #gain coefficient
 k = 100    #photon loss rate
 f = 100     #decadiment rate for spontaneous emission
-#p = 5000     #pump force
-pMin = 1000
-pMax = 10000
+
+pMin = 1000  #pump force
+pMax = 12000
 pSteps = 100
+
+startPoint = [80,80]
+graphLimit = [90, 90]
+
+tMax = 0.5
+integrationSteps = 1000
 
 def makeMilonni(G, k, f, p):
     def ode(state, t=0.):
@@ -25,18 +31,27 @@ def makeMilonni(G, k, f, p):
     return ode
 
 
-fig = plt.figure(figsize=(13, 5));
+fig = plt.figure(figsize=(13, 7));
+#fig = pyplot.gcf()
+fig.canvas.set_window_title('Milonni model')
 plt.subplots_adjust(wspace=0.3, hspace=0.3)
 
-ax1 = plt.subplot(221, xlim=(0, 0.2), ylim=(0, 90))
-plt.title('Milonni model, evolution of $n$ from $80$')
+ax1 = plt.subplot(241, xlim=(0, tMax), ylim=(0, graphLimit[0]))
+plt.title('Evolution of $n$ from $'+str(startPoint[0])+'$')
 plt.xlabel('$t$')
 plt.ylabel('$n$')
 
 line1, = ax1.plot([],[])
 
-ax2 = plt.subplot(222, xlim=(0, 90), ylim=(0, 90))
-plt.title('Milonni model, evolution of $(n,N)$ from $(80,80)$')
+ax1b = plt.subplot(242, xlim=(0, tMax), ylim=(0, graphLimit[1]))
+plt.title('Evolution of $N$ from $'+str(startPoint[1])+'$')
+plt.xlabel('$t$')
+plt.ylabel('$N$')
+
+line1b, = ax1b.plot([],[])
+
+ax2 = plt.subplot(222, xlim=(0, graphLimit[0]), ylim=(0, graphLimit[1]))
+plt.title('Evolution of $(n,N)$ from $('+str(startPoint[0])+','+str(startPoint[1])+')$')
 plt.xlabel('$n$')
 plt.ylabel('$N$')
 
@@ -44,57 +59,70 @@ cumulativeEndStatus = [[],[]];
 line2, = ax2.plot([],[])
 line2b, = ax2.plot([],[], color='red')
 
-ax3 = plt.subplot(223, xlim=(0, 100), ylim=(0, 100))
-plt.title('Vector field')
+ax3 = plt.subplot(223, xlim=(0, graphLimit[0]), ylim=(0, graphLimit[1]))
+#plt.title('Vector field')
 plt.xlabel('$n$')
 plt.ylabel('$N$')
 
-status = np.meshgrid( np.linspace(0,100,20),np.linspace(0,100,20) )
+status = np.meshgrid( np.linspace(0,graphLimit[0],20),np.linspace(0,graphLimit[1],20) )
 endStatus = makeMilonni(G, k, f, pMin)(status)
 q = ax3.quiver(status[0], status[1], endStatus[0], endStatus[1])
 
-ax4 = plt.subplot(224)
+GText = plt.figtext(0.6, 0.40, '')
+kText = plt.figtext(0.6, 0.35, '')
+fText = plt.figtext(0.6, 0.30, '')
+pText = plt.figtext(0.6, 0.25, '')
 
-GText = ax4.text(0.2, 0.95, '', transform=ax4.transAxes)
-kText = ax4.text(0.2, 0.90, '', transform=ax4.transAxes)
-fText = ax4.text(0.2, 0.85, '', transform=ax4.transAxes)
-pText = ax4.text(0.2, 0.80, '', transform=ax4.transAxes)
+plt.figtext(0.68, 0.31, '{', fontsize=40) #size='xx-large')
+plt.figtext(0.7, 0.35, '$\dot{n} = GnN - kn$')
+plt.figtext(0.7, 0.30, '$\dot{N} = -GnN - fN + p$')
 
+pause = False
+
+def onClick(event):
+    global pause
+    pause ^= True
+    
+fig.canvas.mpl_connect('button_press_event', onClick)
 
 def init():
     line1.set_data([], [])
+    line1b.set_data([], [])
     line2.set_data([], [])
     line2b.set_data([], [])
+
     GText.set_text('')
     kText.set_text('')
     fText.set_text('')
     pText.set_text('')
-    return line1, line2, line2b, GText, kText, fText, pText
+
+    return line1, line1b, line2, line2b, GText, kText, fText, pText
 
 def step(G, k, f, pMin, pSteps):
     def realStep(i):#, q, status):
-        if i==0:
-            cumulativeEndStatus[:] = [[],[]]
-    
-        state0 = [80,80]
-        t = np.linspace(0.0, 0.2, 300)
-        state = scipy.integrate.odeint(makeMilonni(G, k, f, pMin+(i*pSteps)), state0, t)
-        line1.set_data(t,state[:,0])
-        cumulativeEndStatus[0].append(state[:,0][-1])
-        cumulativeEndStatus[1].append(state[:,1][-1])
+        if not pause:
+            if i==0:
+                cumulativeEndStatus[:] = [[],[]]
+            t = np.linspace(0.0, tMax, integrationSteps)
 
-        line2.set_data(state[:,0], state[:,1])
-        line2b.set_data(cumulativeEndStatus[0], cumulativeEndStatus[1])
+            state = scipy.integrate.odeint(makeMilonni(G, k, f, pMin+(i*pSteps)), startPoint, t)
+            line1.set_data(t,state[:,0])
+            line1b.set_data(t,state[:,1])
+            cumulativeEndStatus[0].append(state[:,0][-1])
+            cumulativeEndStatus[1].append(state[:,1][-1])
 
-        endStatus = makeMilonni(G, k, f, pMin+(i*pSteps))(status)
-        q.set_UVC(endStatus[0], endStatus[1])
+            line2.set_data(state[:,0], state[:,1])
+            line2b.set_data(cumulativeEndStatus[0], cumulativeEndStatus[1])
 
-        GText.set_text('$G$ = %.1f' % G)
-        kText.set_text('$k$ = %.1f' % k)
-        fText.set_text('$f$ = %.1f' % f)
-        pText.set_text('$p$ = %d' % (pMin+(i*pSteps)))
+            endStatus = makeMilonni(G, k, f, pMin+(i*pSteps))(status)
+            q.set_UVC(endStatus[0], endStatus[1])
 
-        return line1, line2, line2b, cumulativeEndStatus, q, GText, kText, fText, pText
+            GText.set_text('$G$ = %.1f' % G)
+            kText.set_text('$k$ = %.1f' % k)
+            fText.set_text('$f$ = %.1f' % f)
+            pText.set_text('$p$ = %d' % (pMin+(i*pSteps)))
+        
+        return line1, line1b, line2, line2b, cumulativeEndStatus, q, GText, kText, fText, pText
     return realStep
 
 anim = ani.FuncAnimation(fig, step(G, k, f, pMin, pSteps), init_func=init, frames=math.ceil((pMax-pMin)/pSteps), interval=10, blit=False) #, fargs=(q, status)
