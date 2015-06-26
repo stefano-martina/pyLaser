c985685eb010f0c00932681b7401ee144dec1c6c
Stefano Martina <stefano.martina@gmail.com>
2015-06-26 23:30:13 +0200

    added second plot with stability in maxwell adiabatic

--- maxwellBifurcationAdiabatic.py
+++ maxwellBifurcationAdiabatic.py
@@ -1,39 +1,86 @@
 #!/usr/bin/python
 
-#import numpy as np
-#import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import math
 import matplotlib.animation as ani
-from pylab import *
+#from pylab import *
 
 #constants
-k = 10    #decay rate in laser cavity (beam trasmission) (>0)
-g1 = 1000   #decay rates of atomic polarization (>0)
-g2 = 1000   #decay rates for population inversion (>0)
+k = 10.    #decay rate in laser cavity (beam trasmission) (>0)
 
-lMin = -2  #pumping energy parameter (in R)
-lMax = 1
+lMin = -2.  #pumping energy parameter (in R)
+lMax = 1.
 lSteps = 0.01
 
-graphLimit = [[-5, 5],[-20, 20]]
+graphLimit = [[-5., 5.],[-20., 20.]]
+graph2YLimit = [-2., 2.]
 
 
-maxwellAdiabaticEl = lambda k, g1, g2, l: lambda E: k*l*(E-E**3)/(l*E*E+1)
+funcE = lambda k, l: lambda E: k*l*(E-E**3)/(l*E*E+1)
+funcEd = lambda k, l: lambda E: k*l*(-l*E**4 - (l+3)*E**2 + 1) / (l*E**2+1)**2
     
 
-fig = plt.figure(figsize=(13, 7));
+fig = plt.figure(figsize=(13, 8));
+plt.subplots_adjust(hspace=0.3)
+
+#up plot
+ax1 = plt.subplot(211, xlim=(graphLimit[0][0], graphLimit[0][1]), ylim=(graphLimit[1][0],graphLimit[1][1]))
 fig.canvas.set_window_title('Stability of Maxwell-Bloch')
 plt.title('Stability study of Maxwell-Bloch equations with adiabatic elimination')
 plt.xlabel('$E$')
 plt.ylabel('$\dot{E}$')
-plt.axhline(0, color='green')
+plt.axhline(0, color='black')
 
-line, = plt.plot([],[])
+line, = ax1.plot([],[], label='$\dot{E} = \kappa\lambda(E-E^3)/(\lambda E^2+1)$')
+lined, = ax1.plot([],[], label='$d/dE [\ \kappa\lambda(E-E^3)/(\lambda E^2+1)\ ]$')
 
-xlim(graphLimit[0])
-ylim(graphLimit[1])
+ax1.legend(handles=[line, lined])
 
-plt.figtext(0.7, 0.80, '$\dot{E} = \kappa\lambda(E-E^3)/(\lambda E^2+1)$')
-lText = plt.figtext(0.7, 0.75, '')
+stable, = ax1.plot([],[],'go')
+unstable, = ax1.plot([],[],'ro')
+
+asymptote, = ax1.plot([],[],'bo')
+
+lText = plt.figtext(0.8, 0.74, '')
+
+#down plot
+ax2 = plt.subplot(212, ylim=(graph2YLimit[0], graph2YLimit[1]))
+plt.axvline(0, color='black')
+
+#plt.title('Stability study of Maxwell-Bloch equations with adiabatic elimination')
+plt.xlabel('$\lambda$')
+plt.ylabel('$E$')
+
+l1 = np.linspace(lMin, -1, 1000, endpoint=False)
+l2 = np.linspace(-1, 0, 1000, endpoint=False)
+l3 = np.linspace(0, lMax, 1000)
+
+fpA = lambda l: -1
+fpB = lambda l: -1/math.sqrt(-l)
+fpC = lambda l: 0
+fpD = lambda l: 1/math.sqrt(-l)
+fpE = lambda l: 1
+
+line2, = ax2.plot([],[], 'black')
+
+ax2.plot(l1, list(map(fpA, l1)), 'g')
+ax2.plot(l2, list(map(fpA, l2)), 'r')
+ax2.plot(l3, list(map(fpA, l3)), 'g')
+
+ax2.plot(l1, list(map(fpB, l1)), 'r')
+ax2.plot(l2, list(map(fpB, l2)), 'g')
+
+ax2.plot(l1, list(map(fpC, l1)), 'g')
+ax2.plot(l2, list(map(fpC, l2)), 'g')
+ax2.plot(l3, list(map(fpC, l3)), 'r')
+
+ax2.plot(l1, list(map(fpD, l1)), 'r')
+ax2.plot(l2, list(map(fpD, l2)), 'g')
+
+ax2.plot(l1, list(map(fpE, l1)), 'g')
+ax2.plot(l2, list(map(fpE, l2)), 'r')
+ax2.plot(l3, list(map(fpE, l3)), 'g')
 
 
 pause = False
@@ -46,8 +93,13 @@ fig.canvas.mpl_connect('button_press_event', onClick)
 
 def init():
     line.set_data([], [])
+    lined.set_data([], [])
+    line2.set_data([], [])
     lText.set_text('')
-    return line, lText
+    stable.set_data([], [])
+    unstable.set_data([], [])
+    asymptote.set_data([], [])
+    return line, lined, line2, lText, stable, unstable, asymptote
 
 def makeGenerator(lMin, lMax, lSteps):
     def generator():
@@ -58,17 +110,41 @@ def makeGenerator(lMin, lMax, lSteps):
             yield l
     return generator
 
-def step(k, g1, g2, lMin, lSteps):
+def step(k, lMin, lSteps):
     def realStep(l):
         E = np.linspace(graphLimit[0][0],graphLimit[0][1], 1000)
-        dE = maxwellAdiabaticEl(k, g1, g2, l)(E)
-        line.set_data(E,dE)
+        fE = funcE(k, l)(E)
+        line.set_data(E,fE)
+        fEd = funcEd(k, l)(E)
+        lined.set_data(E,fEd)
         lText.set_text('$\lambda$ = %.2f' % l)
-        return line, lText
+
+        stableX = []
+        stableY = []
+        unstableX = []
+        unstableY = []
+
+        for root in [-1, 0, 1]:
+            if funcEd(k, l)(root) < 0:
+                stableX.append(root)
+                stableY.append(0)
+            else:
+                unstableX.append(root)
+                unstableY.append(0)
+
+        stable.set_data(stableX, stableY)
+        unstable.set_data(unstableX, unstableY)
+
+        if l < 0:
+            asymptote.set_data([-1/math.sqrt(-l), 1/math.sqrt(-l)], [0.,0.])
+        
+        line2.set_data([l,l],[graph2YLimit[0], graph2YLimit[1]])
+        
+        return line, lined, line2, lText, stable, unstable, asymptote
     return realStep
 
 
-anim = ani.FuncAnimation(fig, step(k, g1, g2, lMin, lSteps), frames=makeGenerator(lMin, lMax, lSteps), init_func=init, interval=20, blit=False, repeat=True)
+anim = ani.FuncAnimation(fig, step(k, lMin, lSteps), frames=makeGenerator(lMin, lMax, lSteps), init_func=init, interval=20, blit=False, repeat=True)
 
 plt.show()
 
